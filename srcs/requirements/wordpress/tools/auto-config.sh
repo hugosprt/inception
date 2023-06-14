281e5e771f678ea96fc31044b82961a9bca5e983
hugo spriet <hugo@spriet.tv>
2023-06-14 16:23:14 +0200

    pas mal

--- srcs/requirements/wordpress/tools/auto-config.sh
+++ srcs/requirements/wordpress/tools/auto-config.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 sleep 10
-if [ ! -e /var/www/wordpress/wp-config.php ]; then
+if [ ! -e /var/www/wordpress/conf/wp-config.php ]; then
     wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
     					--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
