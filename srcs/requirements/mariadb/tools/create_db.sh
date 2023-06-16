2b1a37dff3a19252de8ff692234d8bc4a4649236
hugo spriet <hugo@spriet.tv>
2023-06-16 18:49:00 +0200

    403

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 service mysql start;
-mysql --execute "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+mysql  -uroot --skip-password --execute "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql  -uroot  --skip-password -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql  -uroot --skip-password -e  "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql  -uroot  --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -uroot  -p$SQL_ROOT_PASSWORD  -e "FLUSH PRIVILEGES;"
+echo ee
+mysqladmin -uroot -p$SQL_ROOT_PASSWORD shutdown
+echo ff
+exec mysqld
+echo gg
